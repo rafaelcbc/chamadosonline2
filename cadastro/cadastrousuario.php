cc942e44dca39246f31d5c2b4ae6483e4b459eea
igorbernardocosta <igorbernardocosta@gmail.com>
2017-10-31 03:09:05 -0200

    Implementação do cadastro da coluna perfil e alertas
    Alteração do INSERT para incluir automaticamente o id_perfil com base no que foi selecionado no radio button.
    
    Implementação dos alertas abaixo:
    
    "A senha não confere."
    "Usuário cadastrado com sucesso."

--- cadastro/cadastrousuario.php
+++ cadastro/cadastrousuario.php
@@ -5,30 +5,44 @@ require "../arquivos/conexao.php";
 
 if(isset($_POST['cadastrarusuario'])):
 
-    //$perfil = addslashes(trim($_POST['tipousuario']));
     $cpf = addslashes(trim($_POST['cpf']));
     $nome = addslashes(trim($_POST['nome']));
     $login = addslashes(trim($_POST['login']));
     $email = addslashes(trim($_POST['email']));
     $senha = addslashes(trim($_POST['senha']));
     $repetirsenha = addslashes(trim($_POST['repetirsenha']));
-    $perfil = addslashes(trim($_POST['tipoUsuario']));
+    $perfil = addslashes(trim($_POST['perfil']));
+
+    if ($perfil == "administrador"):
+
+        $perfil = 1;
+
+    else:
+
+        $perfil = 2;
+    
+    endif;
 
     if ($senha != $repetirsenha):
-        echo "<script>alert('A senha não confere.');</script>";
+        $_SESSION['erro'] = "<div class='alert alert-danger alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>A senha não confere.</div>";
+        header("Location: ../pages/admcadusuario.php");
+
+        
     else: 
 
     //Inserindo dados no bd
-    $sqlcadusuario=$pdo->prepare ("INSERT INTO tb_usuario (Nome, cpf, login, senha, email, id_perfil) VALUES (?, ?, ?, ?, ?, ?);");
-    $sqlcadusuario->bindValue(1,$nome);
-    $sqlcadusuario->bindValue(2,$cpf);
-    $sqlcadusuario->bindValue(3,$login);
-    $sqlcadusuario->bindValue(4,$senha);
-    $sqlcadusuario->bindValue(5,$email);
-    $sqlcadusuario->bindValue(6,$perfil);
-    $sqlcadusuario->execute();
+        $sqlcadusuario=$pdo->prepare ("INSERT INTO tb_usuario (Nome, cpf, login, senha, email, id_perfil) VALUES (?, ?, ?, ?, ?, ?);");
+        $sqlcadusuario->bindValue(1,$nome);
+        $sqlcadusuario->bindValue(2,$cpf);
+        $sqlcadusuario->bindValue(3,$login);
+        $sqlcadusuario->bindValue(4,$senha);
+        $sqlcadusuario->bindValue(5,$email);
+        $sqlcadusuario->bindValue(6,$perfil);
+        $sqlcadusuario->execute();
 
-    echo "Usuário cadastro com sucesso";
+        $_SESSION['erro'] = "<div class='alert alert-success alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Usuário cadastrado com sucesso.</div>";
+        header("Location: ../pages/admcadusuario.php");
+
     endif;
 endif;
 
