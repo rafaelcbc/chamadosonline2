76713b9dd00baece1753ab56ba92c761c6bd90d1
Rafael Cardoso <comerciorafael@hotmail.com>
2017-10-31 21:42:51 -0200

    Finalização da Tabela de Chamados Concluídos
    Finalização da tabela de chamados concluídos, que servirá de molde para as demais tabelas do sistema.

--- pages/chamadosconcluidos.php
+++ pages/chamadosconcluidos.php
@@ -138,22 +138,47 @@
                             <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
                                 <thead>
                                     <tr>
-                                        <th>Autor</th>
-                                        <th>Data e Hora de Abertura</th>
-                                        <th>Estado</th>
-                                        <th>Prioridade</th>
-                                        <th>Título</th>
-                                        <th>Descrição</th>
+                                        <th class="text-center">Autor</th>
+                                        <th class="text-center">Data e Hora de Abertura</th>
+                                        <th class="text-center">Estado</th>
+                                        <th class="text-center">Prioridade</th>
+                                        <th class="text-center">Título</th>
+                                        <th class="text-center">Descrição</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <td>Rafael Cardoso</td>
+                                        <td>Rafael de Carvalho Brasileiro Cardoso</td>
                                         <td>24/10/2017 20:53</td>
                                         <td>Aberto</td>
                                         <td>Normal</td>
                                         <td>Troca de Equipamento</td>
-                                        <td><a href="#">Ver mais</a></td>
+                                        <td class="text-center">
+                                            <a href="#" data-toggle="modal" data-target="#myModal">
+                                                <i class="fa fa-eye fa-fw"></i> Ver mais
+                                            </a>
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="myModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                                            <h4 class="modal-title" id="myModalLabel">Descrição</h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <p class="text-justify">
+                                                                Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
+                                                            </p>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                                                        </div>
+                                                    </div>
+                                                    <!-- /.modal-content -->
+                                                </div>
+                                                <!-- /.modal-dialog -->
+                                            </div>
+                                            <!-- /.modal -->
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td>Igor Bernardo</td>
@@ -161,7 +186,11 @@
                                         <td>Em análise</td>
                                         <td>Urgente</td>
                                         <td>Troca de Equipamento</td>
-                                        <td><a href="#">Ver mais</a></td>
+                                        <td class="text-center">
+                                            <a href="#" data-toggle="modal" data-target="#myModal">
+                                                <i class="fa fa-eye fa-fw"></i> Ver mais
+                                            </a>
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td>Judith Ribeiro</td>
@@ -169,7 +198,11 @@
                                         <td>Reaberto</td>
                                         <td>Alta</td>
                                         <td>Troca de Equipamento</td>
-                                        <td><a href="#">Ver mais</a></td>
+                                        <td class="text-center">
+                                            <a href="#" data-toggle="modal" data-target="#myModal">
+                                                <i class="fa fa-eye fa-fw"></i> Ver mais
+                                            </a>
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
--- pages/teste.php
+++ pages/teste.php
@@ -30,15 +30,42 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
-                <button onclick="usuarioSenhaInvalidos();">Teste</button>
-                <div class="teste"></div>
+                <!-- Button trigger modal -->
+                <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                    Modal
+                </button>
+                <a href="#" data-toggle="modal" data-target="#myModal">
+                    Modal
+                </a>
+                <!-- Modal -->
+                <div class="modal fade" id="myModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                <h4 class="modal-title" id="myModalLabel">Alteração de Primeiro Acesso</h4>
+                            </div>
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Digite Sua Nova Senha" name="senha" type="password" value="" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Repetir Sua Nova Senha" name="repetirsenha" type="password" value="">
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                                <button type="button" class="btn btn-primary">Salvar</button>
+                            </div>
+                        </div>
+                        <!-- /.modal-content -->
+                    </div>
+                    <!-- /.modal-dialog -->
+                </div>
+                <!-- /.modal -->
             </div>
         </div>
     </div>
-
-    
-    <!-- Validações em JavaScript -->
-    <script src="../js/validacoes.js"></script>
     
     <!-- jQuery -->
     <script src="../vendor/jquery/jquery.min.js"></script>
