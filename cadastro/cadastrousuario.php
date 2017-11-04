b604e7638dee864448252d1cfb045ab1d8e4bcfc
igorbernardocosta <igorbernardocosta@gmail.com>
2017-11-04 01:12:06 -0200

    Novos campos adcionados no cadastro
    Novos campos adcionados no cadastro de usuário.

--- cadastro/cadastrousuario.php
+++ cadastro/cadastrousuario.php
@@ -7,14 +7,16 @@ require "../arquivos/conexao.php";
 if(isset($_POST['cadastrarusuario'])):
     
     //Caso sim, executa os códigos abaixo.
-
-    $cpf = addslashes(trim($_POST['cpf']));
     $nome = addslashes(trim($_POST['nome']));
     $login = addslashes(trim($_POST['login']));
     $email = addslashes(trim($_POST['email']));
     $senha = addslashes(trim($_POST['senha']));
     $repetirsenha = addslashes(trim($_POST['repetirsenha']));
-    $perfil = addslashes(trim($_POST['perfil']));
+    $perfil = addslashes(trim($_POST['tipodeacesso']));
+    $setor = addslashes(trim($_POST['setores']));
+    $telefone = addslashes(trim($_POST['telefone']));
+    
+    $perfil = strtolower($perfil); //Transforma a string em minuscula.
 
     //Verifica o perfil selecionado e informa o Id_Perfil da tabela tb_perfil.
     if ($perfil == "administrador"):
@@ -27,25 +29,34 @@ if(isset($_POST['cadastrarusuario'])):
     
     endif;
 
+    //Descobrindo o id do setor
+    $sqlidsetor=$pdo->prepare("SELECT id, nome FROM tb_setor WHERE nome = ? ");
+    $sqlidsetor->bindValue(1, $setor);
+    $sqlidsetor->execute();
+
+    $idsetor = $sqlidsetor->fetch(PDO::FETCH_ASSOC);
+
+    endif;
+
     //Verifica se a senha digita foi repetida corretamente.
     if ($senha != $repetirsenha):
-
+        
         //Caso seja diferente, exibe alerta de erro.
         $_SESSION['erro'] = "<div class='alert alert-danger alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>A senha não confere.</div>";
         header("Location: ../pages/admcadusuario.php");
 
-        
     else: 
 
         //Query INSERT - Dados do formulário.
-        $sqlcadusuario=$pdo->prepare ("INSERT INTO tb_usuario (Nome, cpf, login, senha, email, id_perfil, primeiro_acesso) VALUES (?, ?, ?, ?, ?, ?, ?);");
+        $sqlcadusuario=$pdo->prepare ("INSERT INTO tb_usuario (Nome, login, senha, telefone, email, id_Setor, id_perfil, primeiro_acesso) VALUES (?, ?, ?, ?, ?, ?, ?, ?);");
         $sqlcadusuario->bindValue(1,$nome);
-        $sqlcadusuario->bindValue(2,$cpf);
-        $sqlcadusuario->bindValue(3,$login);
-        $sqlcadusuario->bindValue(4,md5($senha));
+        $sqlcadusuario->bindValue(2,$login);
+        $sqlcadusuario->bindValue(3,md5($senha));
+        $sqlcadusuario->bindValue(4,md5($telefone));
         $sqlcadusuario->bindValue(5,$email);
-        $sqlcadusuario->bindValue(6,$perfil);
-        $sqlcadusuario->bindValue(7,true);
+        $sqlcadusuario->bindValue(6,md5($idsetor['id']));
+        $sqlcadusuario->bindValue(7,$perfil);
+        $sqlcadusuario->bindValue(8,true);
         $sqlcadusuario->execute();
 
         //Mensagem de cadastro realizado com sucesso.
