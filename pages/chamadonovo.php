fcd56bcca488e03a70f98790f726e83f8e0e8769
igorbernardocosta <igorbernardocosta@gmail.com>
2017-11-04 01:09:50 -0200

    Implementação - Cadastro de chamado novo
    Implementado o cadastro de novos chamados.

--- pages/chamadonovo.php
+++ pages/chamadonovo.php
@@ -134,20 +134,27 @@
                 <div class="col-lg-12">
                     <div class="col-lg-4">
                         <div class="panel-body">
-                            <form role="form">
+                            <form role="form"action = "../cadastro/cadastrochamado.php" method = "POST">
                                 <fieldset>
                                     <div class="form-group">
-                                        <select class="form-control">
-                                            <option>Setor de Destino</option>
-                                            <option>Recursos Humanos</option>
-                                            <option>Tecnologia</option>
-                                            <option>Financeiro</option>
-                                            <option>Administração</option>
+                                        <select class="form-control" name="setordestino">
+                                            <option hidden>Setor de Destino</option>
+                                            <?php                                         
+                                             require "../arquivos/conexao.php";
+                                        
+                                            //Select - Listar todos setores cadastrados no banco de dados (tb_setor)
+                                            $sqlbuscasetor=$pdo->prepare ("SELECT * FROM tb_setor");
+                                            $sqlbuscasetor->execute();
+                                        
+                                            while($buscasetor=$sqlbuscasetor->fetch(PDO::FETCH_ASSOC)){
+                                                echo "<option>".$buscasetor['nome']."</option>";
+                                            }
+                                        ?> 
                                         </select>
                                     </div>
                                     <div class="form-group">
-                                        <select class="form-control">
-                                            <option>Prioridade</option>
+                                        <select class="form-control" name="prioridade">
+                                            <option hidden>Prioridade</option>
                                             <option>Mínima</option>
                                             <option>Normal</option>
                                             <option>Urgente</option>
@@ -158,11 +165,11 @@
                                         <input class="form-control" placeholder="Título" name="titulo" type="text" autofocus>
                                     </div>
                                     <div class="form-group">
-                                        <textarea class="form-control" rows="5" placeholder="Descreva aqui a sua solicitação."></textarea>
+                                        <textarea name = "descricao" class="form-control" rows="5" placeholder="Descreva aqui a sua solicitação."></textarea>
                                     </div>
                                     <!-- Change this to a button or input when using this as a form -->
                                     <div class="col-lg-6">
-                                        <a href="#" class="btn btn-lg btn-primary btn-block disabled">Enviar</a>
+                                        <input type = "submit" name = "cadastrarchamado" class="btn btn-lg btn-primary btn-block" value = "Cadastrar"></a>
                                     </div>
                                     <div class="col-lg-6">
                                         <a href="#" class="btn btn-lg btn-primary btn-block">Limpar</a>
